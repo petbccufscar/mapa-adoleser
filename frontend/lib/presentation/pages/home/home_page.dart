37bb884731b8260421fbba65b1d03295e4f1d5ae
Vinícius Martins Cotrim <49480518+Cotrims@users.noreply.github.com>
2025-06-03 16:40:18 -0300

    feat: dropdown

--- frontend/lib/presentation/pages/home/home_page.dart
+++ frontend/lib/presentation/pages/home/home_page.dart
@@ -1,9 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:mapa_adoleser/presentation/widgets/dropdown.dart';
 
 import '../../widgets/responsive_menu.dart';
 
 /// Página inicial da aplicação, exibida ao iniciar.
 /// Integra componentes visuais e gerencia o layout da home.
+
+class MyFormPage extends StatefulWidget {
+  @override
+  _MyFormPageState createState() => _MyFormPageState();
+}
+
+class _MyFormPageState extends State<MyFormPage> {
+  String? selectedOption;
+
+  final List<DropdownMenuItem<String>> options = [
+    const DropdownMenuItem(value: '1', child: Text('Opção 1')),
+    const DropdownMenuItem(value: '2', child: Text('Opção 2')),
+    const DropdownMenuItem(value: '3', child: Text('Opção 3')),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: CustomDropdownField<String>(
+        label: 'Categoria',
+        hint: 'Selecione uma categoria',
+        value: selectedOption,
+        items: options,
+        onChanged: (value) {
+          setState(() {
+            selectedOption = value;
+          });
+        },
+        validator: (value) {
+          if (value == null) return 'Por favor, selecione uma opção';
+          return null;
+        },
+      ),
+    );
+  }
+}
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -17,6 +54,8 @@ class HomePage extends StatelessWidget {
     // final themeProvider = Provider.of<ThemeProvider>(context);
     // final isDark = themeProvider.themeMode == ThemeMode.dark;
 
+    String? selectedOption;
+
     return Scaffold(
       appBar: const ResponsiveMenu(),
       body: Center(
@@ -25,7 +64,8 @@ class HomePage extends StatelessWidget {
             Text(
               'Home!',
               style: Theme.of(context).textTheme.bodyLarge,
-            )
+            ),
+            MyFormPage()
           ],
         ),
       ),
