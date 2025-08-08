681f366f8a3a7938f1add7cda9bf81facce7b2d0
Vinifa21 <viniciusfa21@gmail.com>
2025-08-08 15:36:14 -0300

    Lógica de notas location/activity + (requirements, admin)

--- backend/adoleserbackend/apps.py
+++ backend/adoleserbackend/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AdoleserbackendConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'adoleserbackend'
+
+    def ready(self):
+        from . import signals
