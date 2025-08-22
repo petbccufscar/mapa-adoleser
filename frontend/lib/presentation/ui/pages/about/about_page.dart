5e0bf662f9713e072675421f4907110a31ea17c0
MauricioAlonzoBCC <mauricioalonzo15112005@outlook.com>
2025-08-21 22:03:36 -0300

    Sobre Adoleser

--- frontend/lib/presentation/ui/pages/about/about_page.dart
+++ frontend/lib/presentation/ui/pages/about/about_page.dart
@@ -11,7 +11,6 @@ class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final auth = context.watch<AuthProvider>();
-
     final isLoggedIn = auth.isLoggedIn;
 
     return Scaffold(
@@ -19,10 +18,32 @@ class AboutPage extends StatelessWidget {
       endDrawer: ResponsiveUtils.shouldShowDrawer(context)
           ? CustomDrawer(isLoggedIn: isLoggedIn)
           : null,
-      body: const Center(
-        child: Text(
-          'Sobre!',
-          style: TextStyle(fontSize: 24),
+      body: Center(
+        child: SizedBox(
+          width: 900, // Limita a largura do texto a no máximo 600 pixels
+          child: Padding(
+            padding: EdgeInsets.all(16.0), // Espaço interno para melhorar a leitura
+            child: 
+            Column(
+              children: [
+                SizedBox(height : 100),
+                Text(
+                  'O AdoleSer é um núcleo pensado para o cuidado integral de adolescentes e suas famílias, composto por uma médica hebiatra, uma psiquiatra, 1 professora de enfermagem e 1 professora de terapia ocupacional da Universidade Federal de São Carlos.',
+                  style: TextStyle(fontSize: 24),
+                  textAlign: TextAlign.justify,
+                ),
+                SizedBox(height : 60),
+                Text(
+                  'Entre suas ações, está a discussão de assuntos relacionados a adolescência, o apoio a rede para os atendimentos e a participação e coordenação do grupo Juntos pelas Adolescências, um grupo intersetorial que tem por objetivo discutir casos complexos, fornecer suporte em rede e pensar em propostas de melhoria para a população adolescente de São Carlos.',
+                  style: TextStyle(fontSize: 24),
+                  textAlign: TextAlign.justify,
+                ),
+                SizedBox(height : 40),
+                Image.asset('assets/images/megafonetexto.png',
+                scale: 1)
+              ],
+            )
+          ),
         ),
       ),
     );
